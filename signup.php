64b23de0faf2aa436e5f1316732b9787f4d40e80
Andrea Acosta <aacosta@cs.loyola.edu>
2018-04-26 00:10:47 -0400

    Added php for new hw and edited change pass file

--- signup.php
+++ signup.php
@@ -18,7 +18,7 @@
 
         $conn= new mysqli($servername, $username, $password, $dbname);
         if($conn->connect_error){
-        die("connection failed: " . $$conn->connect_error);
+        die("connection failed: " . $conn->connect_error);
         }
 
         $sql= "INSERT INTO aacosta_user (username,encryptedPassword) VALUES ('$enteredUsername','$encryptedPassword')";
